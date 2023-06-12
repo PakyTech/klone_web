1ceb6f0bd7c86de10d698fbddded29949f687259
kyeltech <zikel1996@gmail.com>
2023-06-12 21:22:17 +0100

    modify components

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,6 +6,10 @@ const Header = () => {
   return (
     <>
       <div className="header">
+        <main className="landing">
+          <div className="logo"> 
+            <h1>hello</h1>
+          </div>
         {images.map((image) => {
           return (
             <Link href={`/${image.id}`} key={image.id}>
@@ -14,19 +18,22 @@ const Header = () => {
                 style={{
                   backgroundImage: `url(${image.path})`,
                   height: "100vh",
-                  width: "337px",
+                  width: "45vh",
+                  backgroundBlendMode:'overlay',
+                  backgroundColor: '#383838'
                 }}
               >
-                <div className="image-details">
+                <div className="image-details justify-text-left">
                   <div className="services">
                     <h2>{image.service}</h2>
                   </div>
-                  <div className="count">
-                    <h3 className="fs-1 fw-bold">{image.number}</h3>
+                  <div className="count ">
+                    <h3 className="fs-1 fw-bold text-left">{image.number}</h3>
                     <h5>
-                      {image.service === "Business"
-                        ? image.service + "es"
-                        : image.service + "s"}
+                      {image?.number > 1 && image?.service === 'Business'
+                        ? image.service.concat("es")
+                        : image.service.concat("s")
+                        }
                     </h5>
                   </div>
                 </div>
@@ -34,6 +41,7 @@ const Header = () => {
             </Link>
           );
         })}
+        </main>
       </div>
     </>
   );
