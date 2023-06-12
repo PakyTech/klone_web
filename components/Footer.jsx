e101ffa45e56eae556a18591708b5129316fdcfd
kyeltech <zikel1996@gmail.com>
2023-06-12 21:21:20 +0100

    implement footer icon and modify footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,16 +1,59 @@
+import FacebookIcon from "@/public/assets/images/js/socials/FacebookIcon";
+import InstagramIcon from "@/public/assets/images/js/socials/InstagramIcon";
+import LogoIcon from "@/public/assets/images/js/socials/LogoIcon";
+import TwitterIcon from "@/public/assets/images/js/socials/TwitterIcon";
+import Link from "next/link";
 import React from "react";
 import { BsFacebook } from "react-icons/bs";
 
-const Footer = () => {
-  
+const Footer = ({colorTheme}) => {
+
+  let Links = [
+      
+    {
+      name: 'logo',
+      icon: (
+        <LogoIcon color={colorTheme === 'light' ? 'white' : '#000000'}/>
+      )
+
+    },
+    {
+      name: 'instagram',
+      link: 'https://www.instagram.com',
+      icon: (
+        <InstagramIcon color={colorTheme === 'light' ? 'white' : '#000000'}/>
+      )
+
+    },
+    {
+      name: 'twitter',
+      link: 'https://www.instagram.com',
+      icon: (
+        <TwitterIcon color={colorTheme === 'light' ? 'white' : '#000000'}/>
+      )
+
+    },
+    {
+      name: 'facebook',
+      link: 'https://www.instagram.com',
+      icon: (
+        <FacebookIcon color={colorTheme === 'light' ? 'white' : '#000000'}/>
+      )
+
+    }
+  ]
   return (
     <div className="container-fluid">
       <div className="footer-info">
         <div className="logo-icon">
-          <h3>logo</h3>
+          {Links.slice(0,1).map((icon, index) => (
+            <a key={index}>
+              {icon.icon}
+            </a>
+          ))}
         </div>
         <div className="footer-link">
-          <ul>
+          <ul className="">
             <li>
               <a href="/reservation">Reservation Guarantees</a>
             </li>
@@ -22,12 +65,23 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="social-media">
-          <ul>
+        <div className="social-media flex space-x-8">
+          {Links.slice(1,4).map((link, index) => (
+            <a
+            href={link.link}
+            key={index}
+            className=""
+            target={'_blank'}
+            rel="noreferrer"
+            >
+              {link.icon}
+            </a>
+          ))}
+          {/* <ul>
             <li><a href="/instagram">{BsFacebook}</a></li>
             <li>B</li>
             <li>C</li>
-          </ul>
+          </ul> */}
         </div>
       </div>
     </div>
