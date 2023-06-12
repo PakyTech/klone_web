a93bb8a9c43575841a43b275929c1a50021e286c
kyeltech <zikel1996@gmail.com>
2023-06-12 21:20:37 +0100

    change image folder location

--- images.js
+++ images.js
@@ -1,26 +1,26 @@
 export const images = [
   {
     id: "1",
-    path: "/images/mini-mart2.png",
+    path: "assets/images/mini-mart2.png",
     service: "Mini-Mart",
-    number: "32"
+    number: "32",
   },
   {
     id: "2",
-    path: "/images/saloon-img.png",
+    path: "assets/images/saloon-img.png",
     service: "Saloon",
-    number: "65"
+    number: "65",
   },
   {
     id: "3",
-    path: "/images/spa.png",
+    path: "assets/images/spa.png",
     service: "Spa",
-    number: "30"
+    number: "30",
   },
   {
     id: "4",
-    path: "/images/business-2.png",
+    path: "assets/images/business-2.png",
     service: "Business",
-    number: "110"
+    number: "110",
   },
 ];
